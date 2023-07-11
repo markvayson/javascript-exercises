bd132cd34aaec1d9d8ed2a23ed1626989bbdfd06
markvayson <markvayson@gmail.com>
2023-07-11 23:30:22 +0400

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
+};
+const sum = function (obj) {
+  let sum = 0;
+  for (let key in obj) {
+    sum += obj[key];
+  }
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function (...numbers) {
+  let mul = numbers.reduce((total, num) => total * num, 1);
+  return mul;
 };
 
-const multiply = function() {
-
+const power = function (x, y) {
+  return x ** y;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = 1;
+  if (num === 0 || num === 1) {
+    return result;
+  }
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
